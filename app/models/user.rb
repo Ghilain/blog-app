d22bd1dbd5f24c4a2109521569c4f547721e7ebd
Ghilain <ishimweghilain5050@gmail.com>
2022-08-19 13:40:34 +0200

    Add user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :posts, class_name: 'Post'
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
