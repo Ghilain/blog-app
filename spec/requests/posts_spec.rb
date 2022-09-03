d6e56c5d59b881bd09c0451eaf533ff5ea0e857c
Ghilain <ishimweghilain5050@gmail.com>
2022-09-03 20:47:24 +0200

    controller spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) do
+      get '/users/1/posts'
+    end 
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts for user')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) do
+      get '/users/1/posts/1'
+    end 
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Here is  the content about Post for user')
+    end
   end
 end
