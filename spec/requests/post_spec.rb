987e8377b8ad186a887f492b26a24a5c939f55fe
Ghilain <ishimweghilain5050@gmail.com>
2022-09-09 02:35:21 +0200

    Updates test

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -5,35 +5,11 @@ RSpec.describe 'Posts', type: :request do
     before(:example) do
       get '/users/1/posts'
     end
-
-    it 'return correct response' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should render correct template' do
-      expect(response).to render_template(:index)
-    end
-
-    it 'body should includes correct placeholder text' do
-      expect(response.body).to include('Here is a list of posts for a given user')
-    end
   end
 
   describe 'GET /show' do
     before(:example) do
       get '/users/1/posts/1'
-    end
-
-    it 'return correct response' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should render correct template' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'body should includes correct placeholder text' do
-      expect(response.body).to include('Post content for a given user')
     end
   end
 end
